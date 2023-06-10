0a6b28b6f7f193c52c86d7cf4ddbb28682bebb50
Neptune <aparisboske@gmail.com>
2023-06-09 23:28:56 -0400

    started dark mode and finished episodic filtering

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,16 +2,29 @@ import React from 'react'
 import "./Header.css"
 
 function Header() {
+  //create a variable for dark-mode
+  const darkMode = false;
+
   return (
-    <div className="header-container">
+    <div className={darkMode?
+      "header-container header-dark":"header-container"}>
       <div>
         <a href="/" style={{marginRight:"10px"}}>Home</a>
         <a href="/about" style={{marginRight:"10px"}}>About</a>
         <a href="/episodes">Episodes</a>
       </div>
-      <button className="theme-button">Dark Mode</button>
+      <button className="theme-button">
+        {
+          darkMode?
+          "Light Mode"
+          :
+          "Dark Mode"
+        }
+        </button>
     </div>
   )
 }
 
 export default Header
+//https://player.vimeo.com/video/831635385?h=ead7a9e65e&amp;badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479
+//12:11
--- src/pages/Episodes/Episodes.jsx
+++ src/pages/Episodes/Episodes.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
 import './Episodes.css'
 import axios from 'axios'
+import CharacterCard from '../../components/CharacterCard/CharacterCard'
+CharacterCard
 
 function Episodes() {
   //create state for episode options
   const [options, setOptions] = React.useState([])
   const [selectedOption, setSelectedOption] = React.useState(1)
+  const [selectedEpisode, setSelectedEpisode] = React.useState([])
+  const [characterList, setCharacterList] = React.useState([])
 
   // when page loads I need to create the dropdown UI
   //https://rickandmortyapi.com/api/episode
@@ -15,14 +19,14 @@ function Episodes() {
       //make api call to find out how many episodes
       axios.get(`https://rickandmortyapi.com/api/episode`)
       .then(res => {
-        console.log(res.data.info.count)
+        // console.log(res.data.info.count)
         //I need to create an array of numbers
         const epOptions = []
         for (let i = 1; i <= res.data.info.count; i++)
         {
           epOptions.push(i)
         }
-        console.log(epOptions)
+        // console.log(epOptions)
         //store this in state
         setOptions(epOptions)
       })
@@ -33,15 +37,55 @@ function Episodes() {
 
   //function to call when I select episode
   const handleSelectChange = (e) =>{
-    console.log(e.target.value)
+    // console.log(e.target.value)
     //save this value in state
-    selectedOption(e.target.value)
+    setSelectedOption(e.target.value)
 
   }
+
+  React.useEffect(
+    ()=>{
+      console.log(selectedOption)
+      //i need to get data from this episode
+      // https://rickandmortyapi.com/api/episode/28
+      // but then i need to make an api call for each character in the episode
+      //use async
+      //async function returns a promise
+      const fetchEpisodeData = async () =>{
+        try{
+          //get specific episode data
+          const res = await axios.get(`https://rickandmortyapi.com/api/episode/${selectedOption}`)
+          console.log(res.data)
+          //store in state
+          setSelectedEpisode(res.data)
+
+          //res.data.characters is array with all endpoints for characters in this episode
+          const episodeCharacters = await Promise.all(
+            res.data.characters.map(url => {
+              return axios.get(url).then(res => res.data)
+            })
+          )
+            console.log(episodeCharacters)
+            //store in state
+            setCharacterList(episodeCharacters)
+
+        }
+
+        catch(err){
+          console.log(err)
+        }
+      }
+
+      //remember to call it
+      fetchEpisodeData()
+
+    }, [selectedOption]
+  )
+
   return (
     <div className="episodes-container">
       <div>
-        <label>Select an episode</label>
+        <label>Select an episode:</label>
         <select id='select-episode' onChange={handleSelectChange}>
           {
             options.map(item =><option key={item} value={item}>{`Episode ${item}`}</option>)
@@ -49,7 +93,17 @@ function Episodes() {
         </select>
       </div>
       <div>
-
+          <div className='episode-info'>
+            <p>Episode Name: {selectedEpisode?.name}</p>
+            <p>Air Date: {selectedEpisode?.air_date}</p>
+          </div>
+          <div className='character-container'>
+            {
+              characterList.map(item=><CharacterCard
+                key={item.id}
+                character={item} />)
+            }
+          </div>
       </div>
     </div>
   )
@@ -58,4 +112,4 @@ function Episodes() {
 export default Episodes
 
 //https://player.vimeo.com/video/830027451?h=4a4221a6cb&amp;badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479
-//31:13
+//
